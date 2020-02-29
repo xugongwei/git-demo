64add4a5cc2aa5e0ec55dde4418cfb9454396ead
gongwei.xu <gongwei.xu@jiangrongxin.com>
2020-02-29 20:10:39 +0800

    学生类添加两个注解

--- src/main/java/com/example/gitdemo/entity/StudentEntity.java
+++ src/main/java/com/example/gitdemo/entity/StudentEntity.java
@@ -1,8 +1,12 @@
 package com.example.gitdemo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentEntity {
 
     private Long id;
