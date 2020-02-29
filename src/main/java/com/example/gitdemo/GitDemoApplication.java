1765d9ae45b9024c8b821838ea741225ecb60c88
gongwei.xu <gongwei.xu@jiangrongxin.com>
2020-02-29 19:41:30 +0800

    启动类添加注释

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主启动类
+ */
 @SpringBootApplication
 public class GitDemoApplication {
 
